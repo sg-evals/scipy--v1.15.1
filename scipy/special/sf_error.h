17b82484a2e4fae7b5e6d5fc16629ecc5cbd0495
sg-benchmarks <benchmarks@sourcegraph.com>
2026-02-22 23:46:46 +0000

    Mirror of scipy/scipy at v1.15.1
    Upstream: https://github.com/scipy/scipy
    Ref: v1.15.1

---
+++ scipy/special/sf_error.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "sf_error_state.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char *sf_error_messages[];
+
+void sf_error(const char *func_name, sf_error_t code, const char *fmt, ...);
+
+void sf_error_check_fpe(const char *func_name);
+
+#ifdef __cplusplus
+}
+#endif
